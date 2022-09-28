949985c7db80a5aab17b2d6f433f4ed7c6ebff0f
GuSimpson <sgu8890@naver.com>
2022-09-28 11:07:52 +0900

    First - detail - Work

--- Test_Branch/project/First.h
+++ Test_Branch/project/First.h
@@ -1,5 +1,18 @@
 #pragma once
 class First
 {
-};
+public:
+	First(const int n1, const int n2)
+		: num1(n1), num2(n2)
+	{
 
+	};
+	virtual	~First() = default;
+
+	virtual void Begin() = 0;
+	virtual void End() = 0;
+
+private:
+	int num1; 
+	int num2;
+};
